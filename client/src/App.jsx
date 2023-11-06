93db963fd076c33c2d19a0e51532aa3c1f817b55
Abhishek-M-K <khandareabhishek98@gmail.com>
2023-11-06 20:51:50 +0530

    change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import IndexPage from "./pages/indexPage.jsx";
+import IndexPage from "./pages/IndexPage.jsx";
 import LoginPage from "./pages/LoginPage";
 import Layout from "./Layout";
 import RegisterPage from "./pages/RegisterPage";
