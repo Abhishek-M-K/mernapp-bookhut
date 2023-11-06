f7d9296a332eee1af8a2c7234303fb1d488bc984
Abhishek-M-K <khandareabhishek98@gmail.com>
2023-11-06 20:47:01 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import SinglePage from "./pages/SinglePage";
 import BookingPage from "./pages/BookingPage";
 import BookedPage from "./pages/BookedPage";
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "https://backsample.onrender.com";
 axios.defaults.withCredentials = true;
 
 function App() {
