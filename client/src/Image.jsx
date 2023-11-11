37d8fb12dcc11923db365078bbdb1a9a0e134417
Abhishek-M-K <khandareabhishek98@gmail.com>
2023-11-12 01:57:31 +0530

    s3 changes

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -2,6 +2,6 @@ export default function Image({ src, ...rest }) {
   src =
     src && src.includes("http://")
       ? src
-      : "https://backsample.onrender.com/uploads" + src;
+      : "https://backsample.onrender.com/uploads/" + src;
   return <img {...rest} src={src} alt={""} />;
 }
